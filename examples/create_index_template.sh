e57de158a7015955b8ed13f8f46b7966794d2775
Yasuhiro Morioka <ymorioka@gmail.com>
2022-06-14 23:06:12 +0900

    update AQL-quiz example

--- examples/create_index_template.sh
+++ examples/create_index_template.sh
@@ -1,2 +1,6 @@
-# create index
+
+# delete index template
+curl -X DELETE localhost:9200/_index_template/template_1
+
+# create index template
 curl -X PUT localhost:9200/_index_template/template_1?pretty -H 'Content-Type: application/json' -d @index_template.json
